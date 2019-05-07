88440a06725b420835e9e7bd1b58645b6298a393
Caleb Callaway <cqcallaw@gmail.com>
2019-05-07 09:22:45 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Smart Pointer Benchmarking
+
+To build:
+
+```
+$ mkdir build && cd build
+$ cmake .. && make -j
+```
+
+To run:
+
+```
+$ ./benchmark
+```
---
+++ main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+#include <chrono>
+#include <assert.h>
+
+void raw_recursive_increment(int* out, uint bound) {
+	if (*out >= bound) {
+		return;
+	} else {
+		(*out)++;
+		raw_recursive_increment(out, bound);
+	}
+}
+
+void sp_recursive_increment(std::shared_ptr<int> out, uint bound) {
+	if (*out >= bound) {
+		return;
+	} else {
+		(*out)++;
+		sp_recursive_increment(out, bound);
+	}
+}
+
+void print_stats(std::vector<int64_t> v) {
+	auto min = std::min_element(v.begin(), v.end());
+	auto max = std::max_element(v.begin(), v.end());
+	auto average = std::accumulate(v.begin(), v.end(), 0.0) / v.size();
+
+	std::cout << "	Min: " << *min << std::endl;
+	std::cout << "	Max: " << *max << std::endl;
+	std::cout << "	Average: " << average << std::endl;
+}
+
+int main()
+{
+	const uint iterations = 1024;
+	const uint bound = 8192;
+
+	auto raw_ptr = new int();
+	auto raw_results = std::vector<int64_t>(iterations);
+	for (uint i = 0; i < iterations; i++) {
+		*raw_ptr = 0;
+		auto begin = std::chrono::high_resolution_clock::now();
+		raw_recursive_increment(raw_ptr, bound);
+		auto end = std::chrono::high_resolution_clock::now();
+		auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count();
+		assert(*raw_ptr == bound);
+		raw_results[i] = duration;
+	}
+
+	std::cout << "Raw results (in nanoseconds):" << std::endl;
+	print_stats(raw_results);
+
+	auto sp_results = std::vector<int64_t>(iterations);
+	for (uint i = 0; i < iterations; i++) {
+		auto sp = std::make_shared<int>(0);
+		auto begin = std::chrono::high_resolution_clock::now();
+		sp_recursive_increment(sp, bound);
+		auto end = std::chrono::high_resolution_clock::now();
+		auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count();
+		assert(*sp == bound);
+		sp_results[i] = duration;
+	}
+
+	std::cout << "SP results (in nanoseconds):" << std::endl;
+	print_stats(sp_results);
+}
